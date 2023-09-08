5b396e46791f4fc82c7a745d33022b4b9c358a86
jambolul <jami.naatanen@gmail.com>
2023-09-08 13:50:00 +0300

    -advanced assigments 1 and 2 done

---
+++ ex5/ex5.js
@@ -0,0 +1,20 @@
+// Sample clothes array
+const clothes = [
+  {type: 'shirt', size: 'M', color: 'red'},
+  {type: 'pants', size: 'L', color: 'blue'},
+  {type: 'dress', size: 'S', color: 'red'},
+];
+
+// TODO: Implement the getRedClothes function
+function getRedClothes(clothes) {
+  // TODO: Use the filter function to get red clothing objects
+  const clothesArray = clothes.filter((clothes) => clothes.color === 'red');
+  return clothesArray;
+}
+// Test the getRedClothes function
+const redClothes = getRedClothes(clothes);
+console.log(redClothes);
+
+// Output should be:
+// [ { type: 'shirt', size: 'M', color: 'red' },
+//   { type: 'dress', size: 'S', color: 'red' } ]
